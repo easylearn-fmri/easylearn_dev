963df632e32deda0aca7b7ae72700312809d09d9
Your Name <you@example.com>
2020-03-29 20:01:33 +0800

    easylearn:gui:main and data loading module

--- eslearn/gui_test/easyLearn_data_loading_run.py
+++ eslearn/gui_test/easyLearn_data_loading_run.py
@@ -17,16 +17,20 @@ from easylearn_data_loading_gui import Ui_MainWindow
 
 
 class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, working_directory=None):
         QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
+        # Set working_directory
+        self.working_directory = working_directory
+        if self.working_directory:
+            os.chdir(self.working_directory)
         # set appearance
         self.set_run_appearance()
 
         # initiating
-        self.group_dict = {}
+        self.data_loading = {}
         self.selected_group = None
         self.selected_modality = None
         self.selected_file = None
@@ -102,34 +106,40 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         self.label_file.setStyleSheet("color:white; font-weight: bold") 
 
     def load_configuration(self):
-        """Load configuration
+        """Load configuration, and display groups
         """
-        if self.configuration_file == "":
-            self.configuration_file, filetype = QFileDialog.getOpenFileName(self,  
-                                    "Select configuration file",  
-                                    os.getcwd(), "All Files (*);;Text Files (*.txt)") 
-        else:
-            self.set_quite_appearance()
-            QMessageBox.warning( self, 'Warning', f"Configuration was given!: {self.configuration_file}")
-            self.set_run_appearance()
+        self.configuration_file, filetype = QFileDialog.getOpenFileName(self,  
+                                "Select configuration file",  
+                                os.getcwd(), "Text Files (*.json);;All Files (*);;") 
 
-        # Read configuration
+        # Read configuration_file if already selected
         if self.configuration_file != "": 
         # TODO: 解决中文编码的问题 
-            with open(self.configuration_file, 'r') as config:
+            with open(self.configuration_file, 'r', encoding='utf-8') as config:
                 self.configuration = config.read()
-
-            print(self.configuration)
-
             # Check the configuration is valid JSON, then transform the configuration to dict
             # If the configuration is not valid JSON, then give configuration and configuration_file to ""
             try:
                 self.configuration = json.loads(self.configuration)
+                # If already exists data_loading configuration
+                if self.data_loading != {}:
+                    self.set_quite_appearance()
+                    reply = QMessageBox.question(self, "Data loading configuration already exists", 
+                                                "The data_loading configuration is already exists, do you want to rewrite it with the  loaded configuration?",
+                                                 QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+                    self.set_run_appearance()
+                    if reply == QMessageBox.Yes:  
+                        self.data_loading = self.configuration["data_loading"]
+                else:
+                    self.data_loading = self.configuration["data_loading"]
+
+                self.display_groups()
+                self.display_modalities()
+                self.display_files()
             except json.decoder.JSONDecodeError:
-                self.configuration_file = ""
-                self.configuration = ""
                 self.set_quite_appearance()
-                QMessageBox.warning( self, 'Warning', 'Configuration in configuration file is not valid JSON')
+                QMessageBox.warning( self, 'Warning', f'{self.configuration_file} is not valid JSON')
+                self.configuration_file = ""
                 self.set_run_appearance()
         else:
             self.set_quite_appearance()
@@ -139,11 +149,11 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
     def save_configuration(self):
         """Save configuration
         """
-        if self.configuration != "":
-            self.configuration["data_loading"] = self.group_dict
-            self.configuration = json.dumps(self.configuration)
-            with open(self.configuration_file, 'w') as config:
-                config.write(self.configuration)
+        if self.configuration_file != "":
+            self.configuration["data_loading"] = self.data_loading
+            with open(self.configuration_file, 'w', encoding="utf-8") as config:    
+                # Set ensure_ascii=False to save Chinese correctly.
+                config.write(json.dumps(self.configuration, ensure_ascii=False))
         else:
             self.set_quite_appearance()
             QMessageBox.warning( self, 'Warning', 'Please choose a configuration file first (press button at top left corner)!')
@@ -181,14 +191,14 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         self.set_quite_appearance()
         self.group_name, ok = QInputDialog.getText(self, "Add group", "Please name the group:", QLineEdit.Normal, "group_")  
         self.set_run_appearance()
-        if self.group_name not in self.group_dict.keys():
-            self.group_dict[self.group_name] = {}
+        if self.group_name not in self.data_loading.keys():
+            self.data_loading[self.group_name] = {}
         self.display_groups()
 
     def identify_selected_group(self, index):
         """Identify the selected file in the list_view_groups and display_files the files
         """
-        self.selected_group = list(self.group_dict.keys())[index.row()]
+        self.selected_group = list(self.data_loading.keys())[index.row()]
         self.display_modalities()
         self.display_files()
    
@@ -196,7 +206,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         """
         This function is used to remove selected group
         
-        If exist selected self.selected_group and self.selected_group is in list(self.group_dict.keys),
+        If exist selected self.selected_group and self.selected_group is in list(self.data_loading.keys),
         then remove.
         """
 
@@ -207,7 +217,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
             self.set_run_appearance()
             if reply == QMessageBox.Yes:  
                 # Remove selected group
-                del self.group_dict[self.selected_group]
+                del self.data_loading[self.selected_group]
                 self.selected_group = None
                 self.display_groups()
                 self.display_modalities()
@@ -221,7 +231,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         """
         Remove all selections
         """
-        self.group_dict = {}
+        self.data_loading = {}
         self.selected_group = False
         self.display_groups() 
         self.display_modalities()
@@ -235,8 +245,8 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
             self.set_quite_appearance()
             mod_name, ok = QInputDialog.getText(self, "Add modality", "Please name the modality:", QLineEdit.Normal, "modality_")
             self.set_run_appearance()
-            if not (mod_name in self.group_dict[self.selected_group].keys()):  # avoid clear exist modalites
-                self.group_dict[self.selected_group][mod_name] = {"file":[], "mask": [], "target":[], "covariances": []}  #  must copy the dict, otherwise all modalities are the same.
+            if not (mod_name in self.data_loading[self.selected_group].keys()):  # avoid clear exist modalites
+                self.data_loading[self.selected_group][mod_name] = {"file":[], "mask": [], "target":[], "covariances": []}  #  must copy the dict, otherwise all modalities are the same.
             self.display_modalities()
         else:
             self.set_quite_appearance()
@@ -246,7 +256,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
     def identify_selected_modality(self, index):
         """Identify the selected modality
         """
-        self.selected_modality = list(self.group_dict[self.selected_group].keys())[index.row()]
+        self.selected_modality = list(self.data_loading[self.selected_group].keys())[index.row()]
         self.display_files()
 
     def remove_selected_modality(self):
@@ -257,7 +267,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         namely the selected modality is selected from other groups).
         """    
         if (bool(self.selected_modality) & bool(self.selected_group) & 
-            (self.selected_modality in list(self.group_dict[self.selected_group]))):
+            (self.selected_modality in list(self.data_loading[self.selected_group]))):
             self.set_quite_appearance()
             message = "Remove this modality: " + self.selected_modality + " of " + self.selected_group + "?"
             self.set_run_appearance()
@@ -265,7 +275,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
                                          QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
             if reply == QMessageBox.Yes:     
                 # Remove selected modality for selected group
-                del self.group_dict[self.selected_group][self.selected_modality]
+                del self.data_loading[self.selected_group][self.selected_modality]
                 self.selected_modality = None
                 self.display_modalities()
                 self.display_files()
@@ -279,7 +289,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         Remove all modalities for selected group
         """
         if self.selected_group:
-            self.group_dict[self.selected_group] = {}
+            self.data_loading[self.selected_group] = {}
             self.selected_modality = None
             self.display_modalities()  
             self.display_files()
@@ -295,9 +305,10 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         if (bool(self.selected_group) & bool(self.selected_modality)):
             self.loaded_files, filetype = QFileDialog.getOpenFileNames(self,  
                                     "Select files",  os.getcwd(), 
-                                    "All Files (*);;PDF Files (*.pdf);;Text Files (*.txt)")
+                                    "Nifti Files (*.nii);;Matlab Files (*.mat);;Excel Files (*.xlsx);;Excel Files (*.xls);;Text Files (*.txt);;All Files (*)"
+        )
             
-            self.group_dict[self.selected_group][self.selected_modality]["file"].extend(self.loaded_files)
+            self.data_loading[self.selected_group][self.selected_modality]["file"].extend(self.loaded_files)
             self.display_files()
         else:
             self.set_quite_appearance()
@@ -307,7 +318,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
     def identify_selected_file(self, index):
         """Identify the selected file in the list_view_files
         """
-        self.selected_file = self.group_dict[self.selected_group][self.selected_modality]["file"][index.row()]
+        self.selected_file = self.data_loading[self.selected_group][self.selected_modality]["file"][index.row()]
     
     def remove_selected_file(self):
         """
@@ -319,7 +330,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
                                          QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
             self.set_run_appearance()
             if reply == QMessageBox.Yes:     
-                self.group_dict[self.selected_group][self.selected_modality]["file"] = list(set(self.group_dict[self.selected_group][self.selected_modality]["file"]) - set([self.selected_file]))
+                self.data_loading[self.selected_group][self.selected_modality]["file"] = list(set(self.data_loading[self.selected_group][self.selected_modality]["file"]) - set([self.selected_file]))
                 self.selected_file = None
                 self.display_files()
         else:
@@ -334,7 +345,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         print(self.selected_group)
         print(self.selected_modality)
         if (bool(self.selected_group) & bool(self.selected_modality)):  
-            self.group_dict[self.selected_group][self.selected_modality]["file"] = []
+            self.data_loading[self.selected_group][self.selected_modality]["file"] = []
             self.selected_file = None
             self.display_files() 
     #%% -----------------------------------------------------------------
@@ -343,7 +354,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         """
         Display groups' name in the list view
         """
-        self.slm_group.setStringList(self.group_dict.keys())  
+        self.slm_group.setStringList(self.data_loading.keys())  
         self.listView_groups.setModel(self.slm_group)
     
     def display_modalities(self):
@@ -351,7 +362,7 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         Display modalities' name in the list view
         """
         if bool(self.selected_group):
-            self.slm_modality.setStringList(self.group_dict[self.selected_group].keys())  
+            self.slm_modality.setStringList(self.data_loading[self.selected_group].keys())  
             self.listView_modalities.setModel(self.slm_modality)
         else:
             self.slm_modality.setStringList([])  
@@ -362,8 +373,10 @@ class EasylearnDataLoadingRun(QMainWindow, Ui_MainWindow):
         Display files of the selected modality of the selected group
         """
         if (bool(self.selected_group) & bool(self.selected_modality)):
-            if ((self.selected_modality in list(self.group_dict[self.selected_group].keys()))):
-                self.slm_file.setStringList(self.group_dict[self.selected_group][self.selected_modality]["file"])  
+            if ((self.selected_modality in list(self.data_loading[self.selected_group].keys()))):
+                # Add numpy  to file name
+                file_to_display = [str(i+1) + ":  "+ file_name for (i, file_name) in enumerate(self.data_loading[self.selected_group][self.selected_modality]["file"])]
+                self.slm_file.setStringList(file_to_display)  
                 self.listView_files.setModel(self.slm_file)
             else:
                 self.slm_file.setStringList([])  
--- eslearn/gui_test/easylearn_main_run.py
+++ eslearn/gui_test/easylearn_main_run.py
@@ -2,24 +2,20 @@
 # -*- coding: utf-8 -*-
 """
 Main GUI of the easylearn
-"""
 
 # Author: Chao Li <lichao19870617@gmail.com>
 # License: MIT
+"""
 
 
 import sys
 import os
 import time
 import json
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication,QMainWindow,QFileDialog
 from PyQt5.QtWidgets import *
 from PyQt5 import *
 from PyQt5.QtGui import QIcon, QPixmap, QPalette
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QApplication,QWidget,QVBoxLayout,QListView,QMessageBox
-# from PyQt5.QtCore import QStringListModel
 
 from easylearn_main_gui import Ui_MainWindow
 from easylearn_data_loading_run import EasylearnDataLoadingRun
@@ -37,25 +33,11 @@ class EasylearnMainGUI(QMainWindow, Ui_MainWindow):
 
         # Set appearance
         self.set_run_appearance()
-        logo_style = '''
-            #logo{
-                background-color: black;
-                border: 5px solid white;
-                border-radius: 50px;
-                border-image: url('../logo/easylearn.jpg');
-                }
-                #logo:hover {
-                border-radius: 0px;}
-        '''
-        self.logo.setStyleSheet(logo_style)
-        self.setWindowTitle('EASYLEARN')
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("../logo/easylearn.jpg"))
-        self.setWindowIcon(icon)
 
         # Connecting to functions
         self.choose_working_directory.triggered.connect(self.select_workingdir_fun)
-        self.create_file.triggered.connect(self.initialize_configuration_fun)
+        self.create_configuration_file.triggered.connect(self.initialize_configuration_fun)
+        self.choose_configuration_file.triggered.connect(self.load_configuration_fun)
         self.data_loading.clicked.connect(self.data_loading_fun)
         self.feature_engineering.clicked.connect(self.feature_engineering_fun)
         self.machine_learning.clicked.connect(self.machine_learning_fun)
@@ -67,14 +49,20 @@ class EasylearnMainGUI(QMainWindow, Ui_MainWindow):
 
     
     def set_run_appearance(self):
-        """Set dart style appearance.
+        """Set a appearance for easylearn (title, logo, skin, etc).
+        """
+        qss_logo = """#logo{background-color: black;
+                border: 5px solid white;
+                border-radius: 50px;
+                border-image: url('../logo/easylearn.jpg');
+                }
+                #logo:hover {border-radius: 0px;}
         """
         qss_string_all_pushbutton = """
         #MainWindow{background-color: rgb(50, 50, 50)}
         QPushButton{color: rgb(200,200,200); border: 2px solid rgb(100,100,100); border-radius:10}
         QPushButton:hover {background-color: black; color: white; font-size:20px; font-weight: bold}
-        QPushButton#run{background-color:rgb(100,200,100); color:white; border: 2px solid rgb(100,100,100); border-radius:15} 
-                                       
+        QPushButton#run{background-color:rgb(100,200,100); color:white; border: 2px solid rgb(100,100,100); border-radius:15}                              
         """
         qss_string_run_pushbutton = """
         QPushButton{background-color:rgb(100,200,100); color:white; border: 2px solid rgb(100,100,100); border-radius:15}        
@@ -87,6 +75,11 @@ class EasylearnMainGUI(QMainWindow, Ui_MainWindow):
         qss_string_textbrowser = """
         background-color:rgb(200,200,200); color:black; border: 2px solid rgb(100,100,100); border-radius:15; font-size:20px
         """
+        self.logo.setStyleSheet(qss_logo)
+        self.setWindowTitle('EASYLEARN')
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../logo/easylearn.jpg"))
+        self.setWindowIcon(icon)
         self.setStyleSheet(qss_string_all_pushbutton)
         self.run.setStyleSheet(qss_string_run_pushbutton)
         self.quit.setStyleSheet(qss_string_quit_pushbutton)
@@ -99,20 +92,16 @@ class EasylearnMainGUI(QMainWindow, Ui_MainWindow):
         """
         qss_string_qmessage = """
         QPushButton:hover {background-color: white; color: black}
-
         QPushButton{color:white; border: 2px solid rgb(100,100,100); border-radius:5}
-
         #formLayoutWidget_2{color:white; border: 2px solid rgb(100,100,100); border-radius:9}
-
         #MainWindow{background-color: rgb(50, 50, 50)}
-
         QMessageBox{background-color: gray; color: white}                       
         """
         self.setStyleSheet(qss_string_qmessage)
 
     def select_workingdir_fun(self):
         """
-        This function is used to select the working working_directory
+        This function is used to select the working working_directory, then change directory to this directory.
         """
         #  If has selected working working_directory previously, then I set it as initial working working_directory.
         if self.working_directory == "":
@@ -121,6 +110,11 @@ class EasylearnMainGUI(QMainWindow, Ui_MainWindow):
         else:
             self.working_directory = QFileDialog.getExistingDirectory(self, "Select a working_directory", self.working_directory) 
             self.textBrowser.setText("Current working directory is " + self.working_directory + "\n")
+
+        # If already choose a working directory, change directory to the working directory
+        if self.working_directory != "":
+            os.chdir(self.working_directory)
+
 
     def initialize_configuration_fun(self):
         """Create file to save settings
@@ -131,13 +125,40 @@ class EasylearnMainGUI(QMainWindow, Ui_MainWindow):
         if self.working_directory != "":
             self.configuration_file = os.path.join(self.working_directory, configuration_file_name)
             with open(self.configuration_file, 'w') as configuration_file:
-                config={"data_loading": {}, "features_engineering": {}, "machine_learning": {}, "model_evaluation": {}, "statistical_analysis": {}}
+                config = {"data_loading": {}, "features_engineering": {}, "machine_learning": {}, "model_evaluation": {}, "statistical_analysis": {}}
                 config = json.dumps(config)
                 configuration_file.write(config)
                 self.textBrowser.setText("Configuration file is " + self.configuration_file)
         else:
             self.set_quite_appearance()
-            QMessageBox.warning( self, 'Warning', 'Configuration in configuration file is not valid JSON')
+            QMessageBox.warning( self, 'Warning', f'Please choose a working directory first! (press button at the top left corner)')
+            self.set_run_appearance()
+
+    def load_configuration_fun(self):
+        """Load configuration
+        """
+        self.configuration_file, filetype = QFileDialog.getOpenFileName(self,  
+                                "Select configuration file",  
+                                os.getcwd(), "Text Files (*.json);;All Files (*);;") 
+
+        # Read configuration_file if already selected
+        if self.configuration_file != "": 
+        # TODO: 解决中文编码的问题 
+            with open(self.configuration_file, 'r') as config:
+                self.configuration = config.read()
+            # Check the configuration is valid JSON, then transform the configuration to dict
+            # If the configuration is not valid JSON, then give configuration and configuration_file to ""
+            try:
+                self.configuration = json.loads(self.configuration)
+                self.textBrowser.setText("Configuration file is " + self.configuration_file)
+            except json.decoder.JSONDecodeError:
+                self.set_quite_appearance()
+                QMessageBox.warning( self, 'Warning', f'{self.configuration_file} is not valid JSON')
+                self.configuration_file = ""
+                self.set_run_appearance()
+        else:
+            self.set_quite_appearance()
+            QMessageBox.warning( self, 'Warning', 'Configuration file was not selected')
             self.set_run_appearance()
 
     def data_loading_fun(self):
@@ -146,7 +167,7 @@ class EasylearnMainGUI(QMainWindow, Ui_MainWindow):
         Then, this function will process the data loading.
         """
         print('data_loading_fun')
-        self.data_loading = EasylearnDataLoadingRun()
+        self.data_loading = EasylearnDataLoadingRun(self.working_directory)
         self.data_loading.show()
 
     def feature_engineering_fun(self):
