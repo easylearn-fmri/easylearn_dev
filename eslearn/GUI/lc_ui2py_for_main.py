23c5ecf51d0560ffad4cf4565f8f46b121778a49
LiChao <lichao19870617@gmail.com>
2020-04-20 22:18:10 +0800

    add visualization to main gui

--- eslearn/GUI/lc_ui2py_for_main.py
+++ eslearn/GUI/lc_ui2py_for_main.py
@@ -1,3 +1,9 @@
 import os
-cmd_str = r'pyuic5 -o easylearn_main_gui.py D:\My_Codes\easylearn-fmri\eslearn\GUI\easylearn_main_gui.ui'
-os.system(cmd_str)
+from eslearn.utils.el_call_powershell import PowerShell
+
+if __name__ == '__main__':
+    cmd =  r'pyuic5 -o D:/My_Codes/easylearn-fmri/eslearn/GUI/easylearn_main_gui.py D:\My_Codes\easylearn-fmri\eslearn\GUI\easylearn_main_gui.ui'
+    with PowerShell('GBK') as ps:
+        outs, errs = ps.run(cmd)
+    print('error:', os.linesep, errs)
+    print('output:', os.linesep, outs)
