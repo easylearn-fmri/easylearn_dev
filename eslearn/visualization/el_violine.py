6a66517077072fb80f4a1abe2272e4536ce60424
LiChao <lichao19870617@gmail.com>
2020-04-22 19:51:44 +0800

    Update el_violine.py

--- eslearn/visualization/el_violine.py
+++ eslearn/visualization/el_violine.py
@@ -14,7 +14,7 @@ class ViolinPlot(object):
     """This class is used to plot violin
     """
 
-    def plot(self, data, xlabel=None, ylabel=None, xlabelsize=10, ylabelsize=10, xticklabel=None, yticklabel=None):
+    def plot(self, data, xlabel=None, ylabel=None, xlabelsize=10, ylabelsize=10, xticklabel=None, yticklabel=None, xticklabel_rotation=90):
         """Plot
 
         Parameters:
@@ -34,16 +34,16 @@ class ViolinPlot(object):
         data = list([np.array(d) for  d in data])
         sns.violinplot(data=data, linewidth=1, ax=self.ax)
         # set style for the axes
-        self.set_axis_style(xlabel, ylabel, xlabelsize, ylabelsize, xticklabel, yticklabel)
+        self.set_axis_style(xlabel, ylabel, xlabelsize, ylabelsize, xticklabel, yticklabel, xticklabel_rotation)
         plt.subplots_adjust(bottom=0.15, wspace=0.05)
-        plt.show()
+        # plt.show()
 
-    def set_axis_style(self, xlabel, ylabel, xlabelsize, ylabelsize, xticklabel, yticklabel):
+    def set_axis_style(self, xlabel, ylabel, xlabelsize, ylabelsize, xticklabel, yticklabel, xticklabel_rotation):
         self.ax.get_xaxis().set_tick_params(direction='out')
         self.ax.xaxis.set_ticks_position('bottom')
         if xticklabel:
             self.ax.set_xticks(np.arange(0, len(xticklabel) ))
-            self.ax.set_xticklabels(xticklabel, rotation=90)
+            self.ax.set_xticklabels(xticklabel, rotation=xticklabel_rotation, ha="right")
             # self.ax.set_xlim(0.25, len(xlabel) + 0.75)
         if xlabel:
             self.ax.set_xlabel(xlabel, fontsize=xlabelsize)
@@ -60,3 +60,4 @@ if __name__ == "__main__":
     data = [sorted(np.random.normal(0, std, 100)) for std in range(1, 5)]
     violin = ViolinPlot()
     violin.plot(data)
+    plt.show()
