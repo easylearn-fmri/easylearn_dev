a2b4cd50821b8fee76e80ed7125ad2dab340594d
LiChao <lichao19870617@gmail.com>
2020-05-07 10:38:01 +0800

    update el_combine_csv2excel

--- eslearn/utils/el_combine_csv2excel.py
+++ eslearn/utils/el_combine_csv2excel.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+# 
 """Concatenate multiple csv files into a single excel file
+
+Usage: python el_combine_csv2excel.py $directory_of_csv -do $ directory_of_output
 """
 
 from os import listdir, getcwd
 from os.path import join
 import pandas as pd
+import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='Inputs')
@@ -18,9 +22,17 @@ args = parser.parse_args()
 #====================================
 
 files_csv = listdir(args.dir_csv)
-dataframe_csv = pd.DataFrame([])
-for file in files_csv:
-    dataframe_csv = pd.concat([dataframe_csv, pd.read_csv(join(args.dir_csv,file), header=None)], axis=1)
-dataframe_csv.to_excel(join(args.dir_out, "combined_csv.xlsx"), header=False, index=False)
+files_csv_df = pd.Series(files_csv)
+sub_id = files_csv_df.str.findall('([1-9]\d*)')
+sub_id = [np.int16(id[0]) if id != [] else 999999 for id in sub_id]
+order_sub_id = np.argsort(sub_id)
+sorted_files_csv_df = files_csv_df[order_sub_id]
 
-input("")
+dataframe_csv = pd.DataFrame([])
+for file in sorted_files_csv_df:
+    dataframe_csv = pd.concat([dataframe_csv, pd.read_csv(join(args.dir_csv,file), header=None)], axis=1)
+
+dataframe_csv.columns = [cn.split('.')[0] for cn in sorted_files_csv_df]
+dataframe_csv.to_excel(join(args.dir_out, "combined_csv.xlsx"), index=False)
+
+input("Done!\n")
