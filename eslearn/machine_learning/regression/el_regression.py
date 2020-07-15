7893cb4490fb9b452ae82181f4218c949d2f1f75
dongmengshi <dongmengshi1990@163.com>
2020-07-15 12:02:52 +0800

    regression
    The rest of the regression code

--- eslearn/machine_learning/regression/el_regression.py
+++ eslearn/machine_learning/regression/el_regression.py
@@ -6,7 +6,10 @@ This class is used to perform regression
 from sklearn import linear_model
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
-import numpy as np
+from sklearn.linear_model import ElasticNet
+from sklearn.svm import SVR
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 
 class Regression():
@@ -35,10 +38,40 @@ class Regression():
         
         self.coef_ = reg.coef_
         self.intercept_ = reg.intercept_
+        
+    def elasticnet_regression(self, x, y, l1_ratio, alpha):
+        reg = ElasticNet(l1_ratio=l1_ratio, alpha=alpha, random_state=0)
+        reg.fit(x, y)
+        
+        self.coef_ = reg.coef_
+        self.intercept_ = reg.intercept_
+        
+    def svm_regression(self, x, y, kernel, C, gamma):
+        reg = SVR(kernel=kernel, C=C, gamma=gamma)
+        reg.fit(x, y)
+        
+        if kernel == "linear":
+            self.coef_ = reg.coef_
+            self.intercept_ = reg.intercept_
+        else:
+            self.coef_ = None
+            self.intercept_ = None
+            
+    def gaussian_process(self, x, y, kernel, alpha):
+        reg = GaussianProcessRegressor(kernel=kernel, alpha=alpha, random_state=0)
+        reg.fit(x, y)
+        
+    def random_forest(self, x, y, criterion, n_estimators, max_depth):
+        reg = RandomForestRegressor(criterion=criterion, n_estimators=n_estimators, max_depth=max_depth, random_state=0)
+        reg.fit(x, y)
     
     
 if __name__ == "__main__":
-    reg = Regression()
-    reg.linear_regression(x, y)
-    reg.lasso_regression(x, y, alpha)
-    reg.ridge_regression(x, y, alpha)
+    regr = Regression()
+    regr.linear_regression(x, y)
+    regr.lasso_regression(x, y, alpha)
+    regr.ridge_regression(x, y, alpha)
+    regr.elasticnet_regression(x, y, l1_ratio, alpha)
+    regr.svm_regression(x, y, kernel, C, gamma)
+    regr.gaussian_process(x, y, kernel, alpha)
+    regr.random_forest(x, y, criterion, n_estimators, max_depth)
