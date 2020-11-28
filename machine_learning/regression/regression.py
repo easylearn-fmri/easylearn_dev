54bae7d7b26fca0f90d9a2acb2cf2382fc9c66ff
lichao312214129 <lichao19870617@gmail.com>
2020-11-28 19:20:07 +0800

    Update regression.py

--- machine_learning/regression/regression.py
+++ machine_learning/regression/regression.py
@@ -40,16 +40,20 @@ class Regression(BaseMachineLearning, DataLoader, BaseRegression):
         
        # Get training and test datasets        
         cv = self.method_model_evaluation_ 
-        target_test_all = []
-        pred_prob = []
+        self.target_test_all = []
+        self.pred_prob = []
         self.real_score = []
         weights = []
+        subname = []
         for train_index, test_index in cv.split(self.features_, self.targets_):
             feature_train = self.features_[train_index, :]
             feature_test = self.features_[test_index, :]
             target_train = self.targets_[train_index]
             target_test = self.targets_[test_index]
-            target_test_all.extend(target_test)
+            self.target_test_all.extend(target_test)
+
+            subname_ = self.id_[test_index]
+            subname.extend(subname_)
 
             # Resample
             imbalance_resample = self.method_unbalance_treatment_
@@ -59,20 +63,110 @@ class Regression(BaseMachineLearning, DataLoader, BaseRegression):
             # Fit
             self.fit_(feature_train, target_train)
             self.get_weights_(feature_train, target_train)
+
+            # Predict
             y_prob = self.predict_(feature_test)
             
             # Eval performances
             score = self.metric(target_test, y_prob)  
-
             self.real_score.append(score)
-            pred_prob.extend(y_prob)
+            self.pred_prob.extend(y_prob)
+
             weights.append(self.weights_)          
         
+
+        # Eval performances for all fold
+        # out_name_perf = os.path.join(self.out_dir, "classification_performances.pdf")
+        # all_score = ModelEvaluator().regression_evaluator(
+        #     self.target_test_all, self.pred_prob,
+        #     accuracy_kfold=self.real_score, 
+        #     verbose=1, is_showfig=True, is_savefig=True, legend1='Controls', legend2='Patients', out_name=out_name_perf
+        # )
+
         # Save weight
         self.save_weight(weights, self.out_dir)
 
-        return score
+        # # Statistical analysis
+        # print("Statistical analysis...\n")
+        # self.run_statistical_analysis()
         
+        # # Save outputs
+        # outputs = { "subname": subname, "test_targets": self.target_test_all, "test_probability": self.pred_prob, 
+        #             "accuracy": self.real_score, "pvalue_spec": self.pvalue_score, 
+        # }
+
+        # pickle.dump(outputs, open(os.path.join(self.out_dir, "outputs.pickle"), "wb"))
+
+
+        return self
+    
+    def run_statistical_analysis(self):
+        type_dict = {"Binomial test":self.binomial_test, "Permutation test":self.permutation_test}
+        type_dict[self.method_statistical_analysis_]()
+
+    # TODO: change it to calculate P value of Pearson's correlation coefficient
+    def binomial_test(self):
+        k = np.sum(np.array(self.target_test_all) - np.array(self.pred_label)==0)
+        n = len(self.target_test_all)
+        self.pvalue_acc, sum_prob, prob, randk = el_binomialtest.binomialtest(n, k, 0.5, 0.5)
+        self.pvalue_auc = None
+        self.pvalue_sens = None
+        self.pvalue_spec = None
+        print(f"p value for acc = {self.pvalue_acc:.3f}")
+        return self
+
+    def permutation_test(self):
+        print(f"Permutation test: {self.param_statistical_analysis_} times...\n")
+        
+        self.preprocessing()
+        
+        self.permuted_score = []
+
+        for i in range(self.param_statistical_analysis_):
+            print(f"{i+1}/{self.param_statistical_analysis_}...\n")
+            # Get training and test datasets         
+            
+            permuted_score = []
+            self.target_test_all = []
+            for train_index, test_index in cv.split(self.features_, self.targets_):
+                feature_train = self.features_[train_index, :]
+                feature_test = self.features_[test_index, :]
+                permuted_target_train = self.targets_[train_index][np.random.permutation(len(train_index))]
+                target_train = self.targets_[train_index]
+                target_test = self.targets_[test_index]
+                self.target_test_all.extend(target_test)
+
+                subname_ = self.id_[test_index]
+                subname.extend(subname_)
+
+                # Resample
+                imbalance_resample = self.method_unbalance_treatment_
+                permuted_target_train, target_train = imbalance_resample.fit_resample(permuted_target_train, target_train)
+                print(f"After re-sampling, the sample size are: {sorted(Counter(target_train).items())}")
+                
+                # Fit
+                self.fit_(permuted_target_train, target_train)
+                self.get_weights_(permuted_target_train, target_train)
+
+                # Predict
+                y_prob = self.predict_(feature_test)
+                
+                # Eval performances
+                score = self.metric(target_test, y_prob)  
+                permuted_score.append(score)
+             
+            # Average performances of one permutation
+            self.permuted_score.append(np.mean(permuted_score))
+
+        # Get p values
+        self.pvalue_acc = self.calc_pvalue(self.permuted_score, np.mean(self.real_score))
+        print(f"p value for acc = {self.pvalue_acc:.3f}")
+        return self
+
+    @staticmethod
+    def calc_pvalue(permuted_performance, real_performance):
+        return (np.sum(np.array(permuted_performance) <= np.array(real_performance)) + 1) / (len(permuted_performance) + 1)
+    
 
 if __name__ == "__main__":
     time_start = time.time()
