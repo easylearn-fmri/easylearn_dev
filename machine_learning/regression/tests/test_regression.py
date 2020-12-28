e720133da57db7c3ad90ed24f6874a8568092c39
lichao312214129 <lichao19870617@gmail.com>
2020-12-28 21:08:02 +0800

    Update test_regression.py

--- machine_learning/regression/tests/test_regression.py
+++ machine_learning/regression/tests/test_regression.py
@@ -14,8 +14,8 @@ from eslearn.base import BaseMachineLearning, DataLoader
 from eslearn.machine_learning.regression.regression import Regression
 
 
-# x, y = datasets.make_regression(n_samples=500, n_informative=3, n_features=5, random_state=1)
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=42)
+x, y = datasets.make_regression(n_samples=500, n_informative=3, n_features=5, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=42)
 
 # x1 = pd.DataFrame([])
 # x1["__ID__"] = [i for i in range(500)]
